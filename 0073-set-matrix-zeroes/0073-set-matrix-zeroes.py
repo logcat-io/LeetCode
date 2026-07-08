006bc1addfbf211eeb33766ec60bd8cab9855473
LogCat <82052272+logcat-io@users.noreply.github.com>
2026-07-08 21:15:50 +0900

    Time: 1 ms (94.3%), Space: 20.3 MB (36.11%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -4,44 +4,20 @@ class Solution:
         n = len(matrix)
         m = len(matrix[0])
 
-        dx = [0, 0, 1, -1]
-        dy = [1, -1, 0, 0]
-        sets = set()
+        r = [0] * n
+        c = [0] * m
+
         for i in range(n):
             for j in range(m):
-                if matrix[i][j] != 0 or (i,j) in sets:
-                    continue
-                
-                r, c = i, j
-                sets.add((r, c))
-                while r - 1 >= 0:
-                    if matrix[r-1][c] != 0:
-                        sets.add((r-1,c))
+                if matrix[i][j] == 0:
+                    r[i] = -1
+                    c[j] = -1
 
-                    matrix[r-1][c] =0
-                    r -= 1
-                
-                r, c = i, j
-                while r + 1 < n:
-                    if matrix[r+1][c] != 0:
-                        sets.add((r+1,c))
-
-                    matrix[r+1][c] =0
-                    r += 1
-                r, c = i, j
-                while c + 1 < m:
-                    if matrix[r][c+1] != 0:
-                        sets.add((r,c+1))
-
-                    matrix[r][c+1] =0
-                    c += 1
-                r, c = i, j
-                while c - 1 >= 0:
-                    if matrix[r][c-1] != 0:
-                        sets.add((r,c-1))
-
-                    matrix[r][c-1] =0
-                    c -= 1
+        for i in range(n):
+            for j in range(m):
+                if r[i] == -1 or c[j] == -1:
+                    matrix[i][j] = 0
+      
         """
         Do not return anything, modify matrix in-place instead.
         """
