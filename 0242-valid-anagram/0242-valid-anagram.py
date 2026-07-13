3da56885d75ce266df706c78cf57bf2206288d9a
LogCat <82052272+logcat-io@users.noreply.github.com>
2026-07-13 11:30:52 +0900

    Time: 19 ms (16.17%), Space: 19.4 MB (45.41%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -2,6 +2,20 @@ from collections import Counter
 
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        left = {}
+
+        for sc in s:
+            left[sc] = left.get(sc, 0) + 1
+
+        print(left)
+
+        right = {}
+        for tc in t:
+            right[tc] = right.get(tc,0) +1        
+
+        print(right)
+
+        print(left == right)
         return Counter(s) == Counter(t)
         
 
