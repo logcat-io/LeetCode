0392afc07834f2cde6264336337645aa93cdc179
LogCat <82052272+logcat-io@users.noreply.github.com>
2026-07-13 14:01:48 +0900

    Time: 3 ms (89.58%), Space: 22.7 MB (94.32%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -0,0 +1,6 @@
+from collections import Counter
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        return [i for i, _ in sorted(Counter(nums).items(), key=lambda x: -x[1])[:k]]
+
+        
