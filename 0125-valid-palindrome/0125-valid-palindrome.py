fa41ed84b6f47fe3fb8efa8ea13a61850c52d4b6
LogCat <82052272+logcat-io@users.noreply.github.com>
2026-07-20 15:21:54 +0900

    Time: 12 ms (13.86%), Space: 20.5 MB (22.85%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -4,9 +4,16 @@ class Solution:
         regx = r"[^a-zA-Z0-9]"
         clean = re.sub(regx, '', s).lower()
 
-        lo, hi = 0, len(clean) - 1
+        lo, hi = 0, len(s) - 1
         while lo < hi:
-            if clean[lo] != clean[hi]:
+            if not s[lo].isalnum():
+                lo += 1
+                continue
+            elif not s[hi].isalnum():
+                hi -=1
+                continue
+
+            if s[lo].lower() != s[hi].lower():
                 return False
             lo += 1
             hi -= 1
