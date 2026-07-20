8ea61b1439fa23137a36deee2d49529790e1c8cb
LogCat <82052272+logcat-io@users.noreply.github.com>
2026-07-20 15:22:33 +0900

    Time: 0 ms (100%), Space: 20.6 MB (22.85%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -3,21 +3,6 @@ class Solution:
     def isPalindrome(self, s: str) -> bool:
         regx = r"[^a-zA-Z0-9]"
         clean = re.sub(regx, '', s).lower()
-
-        lo, hi = 0, len(s) - 1
-        while lo < hi:
-            if not s[lo].isalnum():
-                lo += 1
-                continue
-            elif not s[hi].isalnum():
-                hi -=1
-                continue
-
-            if s[lo].lower() != s[hi].lower():
-                return False
-            lo += 1
-            hi -= 1
-        
-        return True
+        return clean == clean[::-1]
         
         
