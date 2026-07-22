7aac7e034956099836e8c641ed5cd6d9eb4729ce
LogCat <82052272+logcat-io@users.noreply.github.com>
2026-07-22 11:38:12 +0900

    [level 2] Title: 할인 행사, Time: 131.45 ms, Memory: 18.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.py
@@ -0,0 +1,11 @@
+from collections import Counter
+
+def solution(want, number, discount):
+    memo = Counter({k:v for k, v in zip(want, number)})
+    n = len(discount)
+    ans = 0
+    for i in range(n - 10 + 1):
+        if Counter(discount[i: i+ 10]) == memo:
+            ans += 1
+    
+    return ans
