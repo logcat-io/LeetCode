4542d0695a1f172d7bb618f8e0786d11ad6b5fa6
LogCat <82052272+logcat-io@users.noreply.github.com>
2026-07-23 15:19:50 +0900

    Time: 35 ms (49.22%), Space: 23.3 MB (12.83%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -3,17 +3,15 @@ from collections import defaultdict
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
         n = len(nums)
-        ans = 0
         memo = defaultdict(int)
-        memo[0] = 1
-        # k = prefix[r+1] - prefix[l]
-        # prefix[r+1] - k = prefix[l]
-        
-        prefix = 0
+        ans = 0
 
+        # k = right - left
+        memo[0] = 1
+        right = 0
         for i in range(n):
-            prefix += nums[i]
-            ans += memo[prefix - k]
-            memo[prefix] += 1
-        
+            right += nums[i]
+            ans += memo[right - k]
+            memo[right] += 1
+
         return ans
