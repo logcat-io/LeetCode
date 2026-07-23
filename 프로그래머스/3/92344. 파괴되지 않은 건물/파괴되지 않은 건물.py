b772067b1704f28bdd5753236ad9417c40bbbe17
LogCat <82052272+logcat-io@users.noreply.github.com>
2026-07-23 16:25:24 +0900

    [level 3] Title: 파괴되지 않은 건물, Time: 525.21 ms, Memory: 135 MB -BaekjoonHub

--- 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.py
+++ 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.py
@@ -1,29 +1,30 @@
 def solution(board, skill):
     n = len(board)
     m = len(board[0])
+    diff = [[0] * (m+1) for _ in range(n+1)]
     
-    diff = [[0 for _ in range(m+1)] for _ in range(n+1)]
-    
-    for t, r1, c1, r2, c2, d in skill:
+    for s in skill:
+        t, r1, c1, r2, c2, d = s
+        
         if t == 1: d *= -1
         
-        diff[r1][c1] += d
-        diff[r1][c2+1] -= d
-        diff[r2+1][c1] -= d
         diff[r2+1][c2+1] += d
+        diff[r2+1][c1] -= d
+        diff[r1][c2+1] -= d
+        diff[r1][c1] += d
     
-    for i in range(n):
-        for j in range(1, m):
-            diff[i][j] += diff[i][j-1]
+    for i in range(n+1):
+        for j in range(m):
+            diff[i][j+1] += diff[i][j]
     
     for i in range(m):
-        for j in range(1, n):
-            diff[j][i] += diff[j-1][i]
-            
-    answer = 0
+        for j in range(n):
+            diff[j+1][i] += diff[j][i] 
+    
+    ans = 0
     for i in range(n):
         for j in range(m):
-            if board[i][j] + diff[i][j] > 0:
-                answer += 1
+            if (board[i][j] + diff[i][j]) > 0:
+                ans += 1
     
-    return answer
+    return ans
