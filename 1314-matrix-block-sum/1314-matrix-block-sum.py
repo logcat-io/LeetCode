b969f2cc9967390437f8c7fa7a67727d72b2caff
LogCat <82052272+logcat-io@users.noreply.github.com>
2026-07-23 15:39:14 +0900

    Time: 4 ms (98.84%), Space: 20.4 MB (75.73%) - LeetHub

--- 1314-matrix-block-sum/1314-matrix-block-sum.py
+++ 1314-matrix-block-sum/1314-matrix-block-sum.py
@@ -3,27 +3,22 @@ class Solution:
         n = len(mat)
         m = len(mat[0])
 
-        s = [[0] * (m+1) for _ in range(n+1)]
-        answer = [[0] * m for _ in range(n)]
+        board = [[0] * (m+1) for _ in range(n+1)]
 
         for i in range(n):
-            r = 0
+            row = 0
             for j in range(m):
-                r += mat[i][j]
-                s[i+1][j+1] = s[i][j+1] + r
-        
+                row += mat[i][j]
+                board[i+1][j+1] = board[i][j+1] + row
+
+        ans = [[0] * m for _ in range(n)]
         for i in range(n):
             for j in range(m):
-                r1 = max(0, i-k)
-                c1 = max(0, j-k)
-                r2 = min(n-1, i+k)
-                c2 = min(m-1, j+k)
-            
-                total = s[r2+1][c2+1]
-                total -= s[r2+1][c1]
-                total -= s[r1][c2+1]
-                total += s[r1][c1]
-                
-                answer[i][j] = total
-        
-        return answer
+                r1, c1 = max(i - k, 0), max(j-k, 0)
+                r2, c2 = min(i + k, n-1), min(j+k, m-1)
+                total = board[r2+1][c2+1]
+                total -= board[r2+1][c1]
+                total -= board[r1][c2+1]
+                total += board[r1][c1] 
+                ans[i][j] = total
+        return ans
