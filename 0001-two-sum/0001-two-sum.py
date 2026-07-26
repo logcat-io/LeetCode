833cf3284f6391c76f300aa367f422a18953ece8
LogCat <82052272+logcat-io@users.noreply.github.com>
2026-07-26 23:03:02 +0900

    Time: 0 ms (100%), Space: 20.6 MB (17.43%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,8 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         memo = {}
-        for i, v in enumerate(nums):
-            if (target - v) in memo:
-                return [i, memo[target-v]]
-            
+
+        for i,v in enumerate(nums):
+            if target - v in memo:
+                return i, memo[target-v]
             memo[v] = i
+
