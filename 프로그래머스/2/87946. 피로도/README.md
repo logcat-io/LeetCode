588261cac561caee20fec3738e94a0aa868f5d24
LogCat <82052272+logcat-io@users.noreply.github.com>
2026-07-28 16:28:40 +0900

    [level 2] Title: 피로도, Time: 18.74 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/README.md
+++ 프로그래머스/2/87946. 피로도/README.md
@@ -1,10 +1,10 @@
 # [level 2] 피로도 - 87946 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87946?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87946) 
 
 ### 성능 요약
 
-메모리: 16.6 MB, 시간: 39.95 ms
+메모리: 11.4 MB, 시간: 18.74 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 07월 28일 16:23:22
+2026년 07월 28일 16:28:35
 
 ### 문제 설명
 
--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -1,3 +1,21 @@
 def solution(k, dungeons):
-    answer = -1
-    return answer
+    n = len(dungeons)
+    visited = [False] * n
+    best = 0
+    
+    def bt(fatigue, count):
+        nonlocal best
+        best = max(best, count)
+        for i in range(n):
+            need, cost = dungeons[i]
+            if visited[i]:
+                continue
+            
+            if fatigue < need:
+                continue
+            
+            visited[i] = True
+            bt(fatigue-cost, count + 1)
+            visited[i] = False
+    bt(k, 0)
+    return best
