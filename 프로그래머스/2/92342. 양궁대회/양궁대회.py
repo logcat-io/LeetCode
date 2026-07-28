54826ecafb90b6c94d8e875b3e2ec5d9057695a1
LogCat <82052272+logcat-io@users.noreply.github.com>
2026-07-28 16:02:37 +0900

    [level 2] Title: 양궁대회, Time: 188.20 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/2/92342. 양궁대회/양궁대회.py
+++ 프로그래머스/2/92342. 양궁대회/양궁대회.py
@@ -2,8 +2,8 @@ import sys
 sys.setrecursionlimit(10 ** 6)
 
 def solution(n, info):
-    best = None
-    best_diff = 0
+    best = []
+    best_diff = -1
     
     def diff_of(ryan):
         ryan_score, apeach_score = 0, 0
@@ -17,38 +17,31 @@ def solution(n, info):
         
         return ryan_score - apeach_score
     
-    def lower_first(cand, ryan):
-        for i in range(10, -1, -1):
-            if cand[i] != ryan[i]:
-                return cand[i] > ryan[i]
-        return False
-    
-    def bt(idx, left, ryan):
+    def bt(pos, start, n, ryan):
         nonlocal best, best_diff
         
-        if idx == 10:
-            cand = ryan[:]
-            cand[10] = left
-            d = diff_of(cand)
-            
-            if d <= 0:
+        if pos == n:
+            diff = diff_of(ryan[:])
+            if diff <= 0:
                 return
-            if best is None or d > best_diff:
-                best, best_diff = cand, d
-            elif best_diff == d and lower_first(cand, best):
-                best, best_diff = cand, d
-            
+            if diff > best_diff:
+                best_diff = diff
+                best = [ryan[:]]
+            elif best_diff == diff:
+                best.append(ryan[:])
             return
         
-        need = info[idx] + 1
-        if need <= left:
-            ryan[idx] = need
-            bt(idx + 1, left - need, ryan)
-            ryan[idx] = 0
-
-        bt(idx+1, left, ryan)
+        for i in range(start, 11):
+            ryan[i] += 1
+            bt(pos + 1, i, n, ryan)
+            ryan[i] -= 1
         
         return best
+
+    result = bt(0,0, n, [0]*11)
     
-    result = bt(0, n, [0]*11)
-    return [-1] if result is None else result
+    if best_diff < 0:
+        return [-1]
+    
+    result.sort(key=lambda arr: arr[::-1], reverse=True)
+    return result[0]
