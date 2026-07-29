ea18195f0079964c2f997e9add2e00fa1710435f
LogCat <82052272+logcat-io@users.noreply.github.com>
2026-07-29 12:18:46 +0900

    Time: 46 ms (51.43%), Space: 19.4 MB (17.38%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.py
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -1,14 +1,4 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
-        min_len = min(len(word1), len(word2))
-        answer = []
-        for i in range(min_len):
-            answer.append(word1[i])
-            answer.append(word2[i])
-        
-        if len(word1) == min_len:
-            answer.append(word2[min_len:])
-        else:
-            answer.append(word1[min_len:])
-        return "".join(answer)
+        return "".join(a + b for a, b in zip_longest(word1, word2, fillvalue=""))
 
